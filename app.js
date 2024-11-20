f483a65c709c0f0a0e7b2b19aa0c8400f09c81f6
CKearns23 <cgagner5@me.com>
2024-11-19 22:28:13 -0500

    updates

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 new Vue({
   el: '#app',
   data: {
-      images: [],
-      galleryImages: [],
-      videoSrc: ''
+      images: [
+      "assets/Media/A Bug's Life.jpg1",
+      "assets/Media/Day At The Beach 1947.jpg",
+      "assets/Media/Growing Wild.jpg",
+      "assets/Media/Our Future.jpg",
+      "assets/Media/Wastin' Time.jpg",
+      "assets/Media/Worn Out.jpg"
+    ],
+      galleryImages: [
+        "assets/Media/A Bug's Life.jpg1",
+        "assets/Media/Day At The Beach 1947.jpg",
+        "assets/Media/Growing Wild.jpg",
+        "assets/Media/Our Future.jpg",
+        "assets/Media/Wastin' Time.jpg",
+        "assets/Media/Worn Out.jpg"
+    ],
+      videoSrc: "assets/Media/artgalleryvideo.mp4"
   },
   created() {
     fetch('assets/data.json')
