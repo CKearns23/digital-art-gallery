fc888dd0420b3e95349c652927d5f2d5b80c23e0
CKearns23 <cgagner5@me.com>
2024-11-20 21:00:05 -0500

    updated content

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ new Vue({
             'assets/Media/Wastin Time.jpg',
             'assets/Media/Worn Out.jpg'
         ],
-        videoSrc: 'assets/Media/artgalleryvideo.mp4'
+        videoSrc: 'assets/Media/artgalleryvideo.mp4',
+        isModalOpen: false,  // Controls if the modal is visible
+        modalImage: ''       // The image to be shown in the modal
     },
     mounted() {
         const swiper = new Swiper('.swiper-container', {
@@ -33,5 +35,21 @@ new Vue({
                 disableOnInteraction: false
             }
         });
+    },
+    methods: {
+        openImageModal(image) {
+            // If the same image is clicked again, close the modal
+            if (this.isModalOpen && this.modalImage === image) {
+                this.closeImageModal();
+            } else {
+                this.modalImage = image;
+                this.isModalOpen = true;
+                document.body.classList.add('modal-open');  // Prevent page scroll when modal is open
+            }
+        },
+        closeImageModal() {
+            this.isModalOpen = false;
+            document.body.classList.remove('modal-open');
+        }
     }
 });
