c04e20fb86a13a712bd7afb4004f66165801ab1f
CKearns23 <cgagner5@me.com>
2024-11-20 20:51:40 -0500

    updated modal

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ new Vue({
         videoSrc: 'assets/Media/artgalleryvideo.mp4'
     },
     mounted() {
-        // Swiper initialization
         const swiper = new Swiper('.swiper-container', {
             slidesPerView: 1,
             spaceBetween: 10,
@@ -35,27 +34,23 @@ new Vue({
             }
         });
 
-        // Get modal and close button
         const modal = document.getElementById("image-modal");
         const closeModal = document.getElementById("close-modal");
 
-        // Add click event to each image in the gallery
         const galleryImages = document.querySelectorAll('.gallery-section img');
         galleryImages.forEach(image => {
             image.addEventListener('click', (e) => {
-                // Set the image source for the modal and display the modal
                 const src = e.target.src;
                 document.getElementById("modal-image").src = src;
                 modal.style.display = "block";
             });
         });
 
-        // When the user clicks the "X" button, close the modal
-        closeModal.addEventListener('click', () => {
+        closeModal.addEventListener('click', (event) => {
             modal.style.display = "none";
+            event.stopPropagation();
         });
 
-        // When the user clicks anywhere outside the modal, close it
         window.addEventListener('click', (event) => {
             if (event.target === modal) {
                 modal.style.display = "none";
